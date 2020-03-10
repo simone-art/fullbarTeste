985369d00a9b1e63323872748fccd611f1ec6906
simone-art <simoneperiodista3@gmail.com>
2020-03-10 13:28:11 -0300

    detalhes finais

---
+++ js/fullbar.js
@@ -0,0 +1,7 @@
+
+// const menu1 = document.getElementById("menu1");
+// menu1.addEventListener('click', function(){
+//     let menuDropdown = document.getElementsByClassName('menu-fullbar')
+//     console.log("menu1")
+
+// })
